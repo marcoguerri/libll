c9590b666314150899246464bb52ae414cbd2a2b
Marco Guerri <marco.guerri.dev@fastmail.com>
2016-07-04 22:05:57 +0200

    Added tests for list_del

--- src/list_test.c
+++ src/list_test.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include "list.h"
 #include "test.h"
 
@@ -20,11 +21,10 @@ test_list_init_creates_list_with_one_node()
             ptr_root->next == NULL &&
             ptr_root->prev == NULL);
     list_destroy(ptr_root);
-
 }
 
 void 
-test_list_insert_at_end_appends_correctly()
+test_list_insert_at_end_appends()
 {
     int el = 0;
     size_t i;
@@ -68,6 +68,7 @@ test_list_len_returns_correct_length()
     return;
 
 fail:
+   list_destroy(ptr_root);
     _assert(0);
 }
 
@@ -75,7 +76,7 @@ fail:
 void test_list_get_returns_correct_value()
 {
     int el = 0;
-    uint64_t num_err = 0, i = 0;
+    uint64_t i = 0;
     
     node_t *ptr_root =  list_init(&el, sizeof(int));
 
@@ -93,16 +94,65 @@ void test_list_get_returns_correct_value()
     return;
 
 fail:
+    list_destroy(ptr_root);
     _assert(0);
     return;
 }
 
+void 
+test_list_del_deletes_root()
+{
+    int el = 100;
+    node_t *ptr_root =  list_init(&el, sizeof(int));
+    ptr_root = list_del(ptr_root, &el, sizeof(int));
+    _assert(ptr_root == NULL && list_len(ptr_root) == 0);
+}
+
+
+void 
+test_list_del_deletes_at_the_end()
+{
+
+    int el1 = 100, el2 = 102, el3 = 102;
+    node_t *ptr_root =  list_init(&el1, sizeof(int));
+    ptr_root = list_insert(ptr_root, &el2, sizeof(int), list_len(ptr_root));
+    ptr_root = list_insert(ptr_root, &el3, sizeof(int), list_len(ptr_root));
+    
+    ptr_root = list_del(ptr_root, &el3, sizeof(int));
+    _assert(list_len(ptr_root) == 2 && 
+            list_search(ptr_root, &el2, sizeof(int)) &&
+            list_search(ptr_root, &el1, sizeof(int)));
+
+    list_destroy(ptr_root);
+}
+
+
+void 
+test_list_del_deletes_in_the_middle()
+{
+
+    int el1 = 100, el2 = 102, el3 = 102;
+    node_t *ptr_root =  list_init(&el1, sizeof(int));
+    ptr_root = list_insert(ptr_root, &el2, sizeof(int), list_len(ptr_root));
+    ptr_root = list_insert(ptr_root, &el3, sizeof(int), list_len(ptr_root));
+    
+    ptr_root = list_del(ptr_root, &el2, sizeof(int));
+    _assert(list_len(ptr_root) == 2 && 
+            list_search(ptr_root, &el1, sizeof(int)) &&
+            list_search(ptr_root, &el3, sizeof(int)));
+
+    list_destroy(ptr_root);
+}
 
 int 
 main(int argc, char *argv[])
 {
     test_list_init_creates_list_with_one_node();
-    test_list_insert_at_end_appends_correctly();
+    test_list_insert_at_end_appends();
     test_list_len_returns_correct_length();
     test_list_get_returns_correct_value();
+    test_list_del_deletes_root();
+    test_list_del_deletes_at_the_end();
+    test_list_del_deletes_in_the_middle();
+    return 0;
 }
